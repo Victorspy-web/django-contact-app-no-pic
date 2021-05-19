bc6c7fd4c8e21c21dd01dedfb5dbdc8794806fa6
Victor Dickson <dicksonsmart0711@gmail.com>
2021-05-19 01:50:57 +0000

    Fixed secret_key exposure

--- contact_project/settings.py
+++ contact_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .SECRET_KEY import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w%1_(9r7df#(ov_g+@-kc1#0s7z8(-ylu*ag*4!q*357@kld9c'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
